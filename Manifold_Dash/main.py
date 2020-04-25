1d1684dd66f09ab79addc0af5fdacda7e71660b1
diecalsa <diecalsa@gmail.com>
2020-04-25 17:24:47 +0200

    Prueba Dash

--- Manifold_Dash/main.py
+++ Manifold_Dash/main.py
@@ -15,6 +15,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
 app.layout = html.Div([
     #Header
     html.Div([
